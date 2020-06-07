7c0c6d6f23aea1e53a9c570b8a2a7c265d17fbce
zhudang <645405513@qq.com>
2020-06-07 19:03:17 +0800

    3232

--- mini_program/pages/show/show.js
+++ mini_program/pages/show/show.js
@@ -6,6 +6,7 @@ Page({
    */
   data: {
      list:{},
+     data1:{},
      list_length:0,
   },
   // onDelete:function(){
@@ -68,11 +69,15 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+           let self=this;
            this.onQuery();
            console.log(options.query)
            const eventChannel=this.getOpenerEventChannel()
            eventChannel.on('accept',function(data){
              console.log(data,"here")
+             self.setData({
+               data1:data
+             })
            })
   },
 
