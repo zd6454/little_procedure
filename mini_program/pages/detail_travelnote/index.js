a1b899cca106d4900508b680d54704005b9d479b
bai <2542274498@qq.com>
2020-06-08 17:05:24 +0800

    改bug

--- mini_program/pages/detail_travelnote/index.js
+++ mini_program/pages/detail_travelnote/index.js
@@ -107,7 +107,6 @@ Page({
         time: time,
         key:1,
         isTouchMove:false,//默认隐藏删除
-        // response:[],//回复
         responseamount:0,
       },
       success: res => {
@@ -184,83 +183,32 @@ Page({
     let self = this
     var time = util.formatTime(new Date());
     const contenxt = e.detail.value;//获取输入的评论信息
-    console.log(contenxt)
-    wx.getSetting({
-      success(res) {
-        console.log(res.authSetting)
-        wx.getUserInfo({
-          success: function (res) {
-            console.log(res.userInfo)
-            self.setData({
-              comment: contenxt,
-              comment_user: res.userInfo,
-              time: time,
-              inputValue: "",
-            })
-            self.onADD(res.userInfo, contenxt);
-          }
-        })
-      }
-    })
+    if(contenxt.comment===""){
+      wx.showToast({
+        title: '内容为空,评论失败',
+        icon:'none'
+      })
+    }else{
+      console.log(contenxt)
+      wx.getSetting({
+        success(res) {
+          console.log(res.authSetting)
+          wx.getUserInfo({
+            success: function (res) {
+              console.log(res.userInfo)
+              self.setData({
+                comment: contenxt,
+                comment_user: res.userInfo,
+                time: time,
+                inputValue: "",
+              })
+              self.onADD(res.userInfo, contenxt);
+            }
+          })
+        }
+      })
+    }
   },
-
-
-  // //手指触摸动作开始 记录起点X坐标
-  // touchstart: function (e) {
-  //   //开始触摸时 重置所有删除
-  //   this.data.comment_user_list.forEach(function (v, i) {
-  //     if (v.isTouchMove)//只操作为true的
-  //       v.isTouchMove = false;
-  //   })
-
-  //   this.setData({
-  //     startX: e.changedTouches[0].clientX,
-  //     startY: e.changedTouches[0].clientY,
-  //     comment_user_list: this.data.comment_user_list
-  //   })
-  // },
-
-  // //滑动事件处理
-  // touchmove: function (e) {
-  //   var that = this,
-  //     index = e.currentTarget.dataset.index,//当前索引
-  //     startX = that.data.startX,//开始X坐标
-  //     startY = that.data.startY,//开始Y坐标
-  //     touchMoveX = e.changedTouches[0].clientX,//滑动变化坐标
-  //     touchMoveY = e.changedTouches[0].clientY,//滑动变化坐标
-  //     //获取滑动角度
-  //     angle = that.angle({ X: startX, Y: startY }, { X: touchMoveX, Y: touchMoveY });
-  //     that.data.comment_user_list.forEach(function (v, i) {
-  //        v.isTouchMove = false
-  //     //滑动超过30度角 return
-  //     if (Math.abs(angle) > 30) return;
-  //     if (i == index) {
-  //       if (touchMoveX > startX) //右滑
-  //         v.isTouchMove = false
-  //       else //左滑
-  //         v.isTouchMove = true
-  //     }
-  //   })
-
-  //   //更新数据
-  //   that.setData({
-  //     comment_user_list: that.data.comment_user_list
-  //   })
-  // },
-
-  // /**
-  // * 计算滑动角度
-  // * @param {Object} start 起点坐标
-  // * @param {Object} end 终点坐标
-  // */
-
-  // angle: function (start, end) {
-  //   var _X = end.X - start.X,
-  //     _Y = end.Y - start.Y
-  //   //返回角度 /Math.atan()返回数字的反正切值
-  //   return 360 * Math.atan(_Y / _X) / (2 * Math.PI);
-  // },
-
 
   //删除事件
   del: function (e) {
@@ -304,12 +252,26 @@ Page({
                         },
                         success(res) {
                           console.log(res);
-                         
                         }
                       })
-                      wx.showToast({
-                        title: '删除成功',
+                      db.collection('response').where({
+                        comment_id: e.currentTarget.dataset.index
+                      }).get({
+                        success:res=>{
+                          console.log(res)
+                          //将response从数据库中删去
+                          res.data.forEach(v=>{
+                            var id=v._id//获取到需要删除的response的comment_id
+                            db.collection('response').doc(id).remove({
+                              success: function (ms) {
+                                wx.showToast({
+                                  title: '删除成功',
+                                })
+                              }
+                          })
                       })
+                    }
+                  })
                     },
                     fail: function (ms) {
                       wx.showToast({
