e7403c3d03fc4281a154b58bc99b7321757bacd4
bai <2542274498@qq.com>
2020-06-09 00:54:04 +0800

    合发布和收藏到个人页面

--- mini_program/pages/me/index.js
+++ mini_program/pages/me/index.js
@@ -4,6 +4,8 @@ var qqmapsdk=new QQMapWX({
 });  
 const util = require('../../utils/util.js');
 const db = wx.cloud.database()
+const DB = wx.cloud.database().collection("travelnotes");
+const DB1 = wx.cloud.database().collection("stategies");
 Page({
 
   /**
@@ -23,6 +25,30 @@ Page({
     ifShowInputbox: false,
     inputVal:"",
     list:[],
+    userInfo: [],
+    tabs: [
+      {
+        id: 0,
+        value: "景点",
+        isActive: true
+      },
+      {
+        id: 1,
+        value: "攻略",
+        isActive: false
+      },
+      {
+        id: 2,
+        value: "游记",
+        isActive: false
+      }
+    ],
+    list_travelnotes:[],
+    list_strategy:[],
+    collect_travelnotes:0,
+    collect_strategy:0,
+    user:[],
+    
     dataList: [{
       id: 'money',
       name: '证件&银行卡&现金',
@@ -143,6 +169,8 @@ Page({
     setTimeout(function(){
       self.getlights();
     },1000)
+    this.init();
+    this.init1();
   },
 
   onShow(){
@@ -545,4 +573,558 @@ addlight:function(){
      })
   },
 
+  // 标题点击事件  从子组件传递过来  标签
+  handleTabsItemChange(e) {
+    // console.log(e);
+    // 1.获取被点击的标题
+    const { index } = e.detail;
+    // 2.修改原数组
+    let { tabs } = this.data;
+    tabs.forEach((v, i) => i === index ? v.isActive = true : v.isActive = false);
+    // 3.赋值到data中
+    this.setData({
+      tabs
+    })
+  },
+ 
+  //初始化
+  init() {
+    var collect_travelnotes = 0;
+    var collect_strategy = 0;
+    //获取用户信息
+    const that = this;
+    wx.getUserInfo({
+      success: function (res) {
+        console.log(res);
+        that.setData({
+          user: res.userInfo,//获取到用户信息
+        })
+      }
+    })
+    //游记
+    DB.get({
+      success: res => {
+        console.log(res.data);
+        // console.log("查询成功", res.data);
+        res.data.forEach(v=>{
+          console.log(v.good.length);
+          if(v.good.length!=0){
+            v.good.forEach(u => {
+              if (u === this.data.user.nickName) {
+                collect_travelnotes = collect_travelnotes + 1;
+              }
+            })
+          }
+        })
+        this.setData({
+          list_travelnotes: res.data,
+          collect_travelnotes: collect_travelnotes,
+          tabs: [
+            {
+              id: 0,
+              value: "景点",
+              isActive: true
+            },
+            {
+              id: 1,
+              value: "攻略*"+this.data.collect_strategy,
+              isActive: false
+            },
+            {
+              id: 2,
+              value: "游记*" + collect_travelnotes,
+              isActive: false
+            }
+          ],
+        })
+        // console.log(this.data.collect_travelnotes);
+      },
+      file(res) {
+        console.log("查询失败", res);
+      }
+    })
+
+    //攻略
+    DB1.get({
+      success: res => {
+        res.data.forEach(v => {
+          console.log(v.good.length);
+          if (v.good.length != 0) {
+            v.good.forEach(u => {
+              if (u === this.data.user.nickName) {
+                collect_strategy = collect_strategy + 1;
+              }
+            })
+          }
+        })
+        // console.log("查询成功", res.data);
+        this.setData({
+          list_strategy: res.data,
+          collect_strategy: collect_strategy,
+          tabs: [
+          {
+            id: 0,
+            value: "景点",
+            isActive: true
+          },
+          {
+            id: 1,
+            value: "攻略*" + collect_strategy,
+            isActive: false
+          },
+          {
+            id: 2,
+            value: "游记*" + this.data.collect_travelnotes,
+            isActive: false
+          }
+        ],
+        })
+        console.log(this.data.collect_strategy);
+      },
+      file(res) {
+        console.log("查询失败", res);
+      }
+    })
+  },
+
+
+  //返回到页面时刷新
+  onShow: function () {
+    this.init();
+    this.init1();
+  },
+  //下拉刷新
+  onPullDownRefresh() {
+    this.init();
+    this.init1();
+    wx.stopPullDownRefresh();
+  },
+
+
+
+  //长按移除  游记
+  longPress_good(e){
+    console.log(e);
+    const note_id = e.currentTarget.dataset.index
+    var list_travelnotes = this.data.list_travelnotes
+    var good;
+    var goodamount = e.currentTarget.dataset.goodamount
+    var that = this;
+    var collect_travelnotes = this.data.collect_travelnotes
+
+    wx.getUserInfo({
+      success: function (res) {
+        console.log(res);
+        list_travelnotes.forEach(v => {
+          // console.log(list_travelnotes)
+          if (note_id === v._id) {
+            wx.showModal({
+              title: '提示',
+              content: '是否确定移除收藏',
+              success(t) {
+                if (t.confirm) {
+                  console.log('用户点击确定',v.good.length);
+                  var i=0;
+                  for (; i < v.good.length; i++) {
+                    console.log("11111111111111")
+                    if (v.good[i] === res.userInfo.nickName) {
+                      console.log("取消收藏")
+                      console.log(v.good)
+                      v.good.splice(i, 1);
+                      console.log(v.good)
+                      v.goodamount = v.goodamount - 1
+                      good = v.good
+                      console.log(good)
+                      goodamount = v.goodamount
+                      collect_travelnotes = collect_travelnotes-1
+                      break;
+                    }
+                  }
+                  that.setData({
+                    list_travelnotes: list_travelnotes,
+                    collect_travelnotes:collect_travelnotes,
+                     tabs: [
+                      {
+                        id: 0,
+                        value: "景点",
+                        isActive: false
+                      },
+                      {
+                        id: 1,
+                        value: "攻略*" + that.data.collect_strategy,
+                        isActive: false
+                      },
+                      {
+                        id: 2,
+                        value: "游记*" + collect_travelnotes,
+                        isActive: true
+                      }
+                    ],
+                  })
+                  console.log(that.data.list_travelnotes)
+
+                  DB.doc(note_id).update({
+                    data: {
+                      good: good,
+                      goodamount: goodamount,
+                    },
+                    success(res) {
+                      wx.showToast({
+                        title: '移除成功',
+                      })
+                    },
+                       fail: function (ms) {
+                      wx.showToast({
+                        title: '移除失败',
+                        icon: "none"
+                      })
+                    },
+                  })
+                } else if (t.cancel) {
+                  console.log('用户点击取消')
+               }
+              }
+            })
+      }
+    })
+   }
+  })
+ },
+
+  //长按移除  攻略
+  longPress_good_str(e) {
+    console.log(e);
+    const strategy_id = e.currentTarget.dataset.index
+    var list_strategy = this.data.list_strategy
+    var good;
+    var goodamount = e.currentTarget.dataset.goodamount
+    var that = this;
+    var collect_strategy = this.data.collect_strategy
+
+    wx.getUserInfo({
+      success: function (res) {
+        console.log(res);
+        list_strategy.forEach(v => {
+          // console.log(list_strategy)
+          if (strategy_id === v._id) {
+            wx.showModal({
+              title: '提示',
+              content: '是否确定移除收藏',
+              success(t) {
+                if (t.confirm) {
+                  console.log('用户点击确定', v.good.length);
+                  var i = 0;
+                  for (; i < v.good.length; i++) {
+                    // console.log("11111111111111")
+                    if (v.good[i] === res.userInfo.nickName) {
+                      console.log("取消收藏")
+                      console.log(v.good)
+                      v.good.splice(i, 1);
+                      console.log(v.good)
+                      v.goodamount = v.goodamount - 1
+                      good = v.good
+                      console.log(good)
+                      goodamount = v.goodamount
+                      collect_strategy = collect_strategy-1
+                      break;
+                    }
+                  }
+                  that.setData({
+                    list_strategy: list_strategy,
+                    collect_strategy: collect_strategy,
+                    tabs: [
+                      {
+                        id: 0,
+                        value: "景点",
+                        isActive: false
+                      },
+                      {
+                        id: 1,
+                        value: "攻略*" + collect_strategy,
+                        isActive: true
+                      },
+                      {
+                        id: 2,
+                        value: "游记*" + that.data.collect_travelnotes,
+                        isActive: false
+                      }
+                    ],
+                  
+                  })
+                  console.log(that.data.list_travelnotes)
+
+                  DB1.doc(strategy_id).update({
+                    data: {
+                      good: good,
+                      goodamount: goodamount,
+                    },
+                    success(res) {
+                      wx.showToast({
+                        title: '移除成功',
+                      })
+                    },
+                    fail: function (ms) {
+                      wx.showToast({
+                        title: '移除失败',
+                        icon: "none"
+                      })
+                    },
+                  })
+                } else if (t.cancel) {
+                  console.log('用户点击取消')
+                }
+              }
+            })
+          }
+        })
+      }
+    })
+  },
+
+  // 标题点击事件  从子组件传递过来  标签
+  handletabs1ItemChange(e) {
+    // console.log(e);
+    // 1.获取被点击的标题
+    const { index } = e.detail;
+    // 2.修改原数组
+    let { tabs1 } = this.data;
+    tabs1.forEach((v, i) => i === index ? v.isActive = true : v.isActive = false);
+    // 3.赋值到data中
+    this.setData({
+      tabs1
+    })
+  },
+
+  //初始化
+  init1() {
+    var release_travelnotes = 0;
+    var release_strategy = 0;
+
+    //获取用户信息
+    const that = this;
+    wx.getUserInfo({
+      success: function (res) {
+        console.log(res);
+        that.setData({
+          user: res.userInfo,//获取到用户信息
+        })
+      }
+    })
+    //游记
+    DB.get({
+      success: res => {
+        console.log(res.data);
+        console.log("查询成功", res.data);
+        res.data.forEach(v => {
+          // console.log(v.good.length);
+          if (v.userInfo.nickName === this.data.user.nickName) {
+                release_travelnotes = release_travelnotes + 1;
+                // console.log("4444444444444444")
+          }
+        })
+        this.setData({
+          list_travelnotes: res.data,
+          release_travelnotes: release_travelnotes,
+          tabs1: [
+            {
+              id: 0,
+              value: "景点",
+              isActive: true
+            },
+            {
+              id: 1,
+              value: "攻略*"+this.data.release_strategy,
+              isActive: false
+            },
+            {
+              id: 2,
+              value: "游记*" + release_travelnotes,
+              isActive: false
+            }
+          ],
+        })
+        console.log("bbbbbbbbbbbbbbbbbb",this.data.release_travelnotes);
+      },
+      file(res) {
+        console.log("查询失败", res);
+      }
+    })
+
+    //攻略
+    DB1.get({
+      success: res => {
+        // console.log("查询成功", res.data);
+        res.data.forEach(v => {
+          // console.log(v.good.length);
+          if (v.userInfo.nickName === this.data.user.nickName) {
+            release_strategy = release_strategy + 1;
+            // console.log("4444444444444444")
+          }
+        })
+        console.log(this.data.release_travelnotes)
+        this.setData({
+          list_strategy: res.data,
+          release_strategy: release_strategy,
+          tabs1: [
+            {
+              id: 0,
+              value: "景点",
+              isActive: true
+            },
+            {
+              id: 1,
+              value: "攻略*" + release_strategy,
+              isActive: false
+            },
+            {
+              id: 2,
+              value: "游记*" + this.data.release_travelnotes,
+              isActive: false
+            }
+          ],
+        })
+       
+      },
+      file(res) {
+        console.log("查询失败", res);
+      }
+    })
+  },
+
+    //长按删除
+    longPress_record(e) {
+      console.log(e);
+      const note_id = e.currentTarget.dataset.index
+      var list_travelnotes = this.data.list_travelnotes
+      var release_travelnotes = this.data.release_travelnotes
+      var that = this;
+  
+      wx.getUserInfo({
+        success: function (res) {
+          console.log(res);
+          list_travelnotes.forEach(v => {
+            if (res.userInfo.nickName === v.userInfo.nickName) {//发布的人可删除
+              wx.showModal({
+                title: '提示',
+                content: '是否确定删除',
+                success(t) {
+                  if (t.confirm) {
+                    console.log('用户点击确定');
+                    //从数据库中删除
+                    DB.doc(note_id).remove({
+                      success: function (ms) {
+                        // var commentamount
+                        //从数组中删除
+                        list_travelnotes.splice(note_id, 1);
+                        release_travelnotes=release_travelnotes-1
+                        that.setData({
+                          release_travelnotes: release_travelnotes,
+                          list_travelnotes: list_travelnotes,
+                          tabs1: [
+                            {
+                              id: 0,
+                              value: "景点",
+                              isActive: false
+                            },
+                            {
+                              id: 1,
+                              value: "攻略*" + that.data.release_strategy,
+                              isActive: false
+                            },
+                            {
+                              id: 2,
+                              value: "游记*" +release_travelnotes,
+                              isActive: true
+                            }
+                          ],
+                        })
+                        wx.showToast({
+                          title: '删除成功',
+                        })
+                      },
+                      fail: function (ms) {
+                        wx.showToast({
+                          title: '删除失败',
+                          icon: "none"
+                        })
+                      },
+                    })
+                  } else if (t.cancel) {
+                    console.log('用户点击取消')
+                  }
+                }
+              })
+            }
+          })
+        }
+      })
+    },
+    //长按删除  攻略
+    longPress_record_str(e) {
+      console.log(e);
+      const strategy_id = e.currentTarget.dataset.index
+      var list_strategy = this.data.list_strategy
+      var release_strategy = this.data.release_strategy
+      var that = this;
+  
+      wx.getUserInfo({
+        success: function (res) {
+          console.log(res);
+          list_strategy.forEach(v => {
+            if (res.userInfo.nickName === v.userInfo.nickName) {//发布的人可删除
+              wx.showModal({
+                title: '提示',
+                content: '是否确定删除',
+                success(t) {
+                  if (t.confirm) {
+                    console.log('用户点击确定');
+                    //从数据库中删除
+                    DB1.doc(strategy_id).remove({
+                      success: function (ms) {
+                        release_strategy = release_strategy-1
+                        // var commentamount
+                        //从数组中删除
+                        list_strategy.splice(strategy_id, 1);
+                        that.setData({
+                          release_strategy: release_strategy,
+                          list_strategy: list_strategy,
+                          tabs1: [
+                            {
+                              id: 0,
+                              value: "景点",
+                              isActive: false
+                            },
+                            {
+                              id: 1,
+                              value: "攻略*" + release_strategy,
+                              isActive: true
+                            },
+                            {
+                              id: 2,
+                              value: "游记*" + that.data.release_travelnotes,
+                              isActive: false
+                            }
+                          ],
+                        })
+                        wx.showToast({
+                          title: '删除成功',
+                        })
+                      },
+                      fail: function (ms) {
+                        wx.showToast({
+                          title: '删除失败',
+                          icon: "none"
+                        })
+                      },
+                    })
+                  } else if (t.cancel) {
+                    console.log('用户点击取消')
+                  }
+                }
+              })
+            }
+          })
+        }
+      })
+    },
 })
