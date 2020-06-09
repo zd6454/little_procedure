a42be937499b49376f89a5b0ac07e39152904a47
bai <2542274498@qq.com>
2020-06-09 15:31:11 +0800

    个人页面的发布和收藏完善

--- mini_program/pages/me/index.js
+++ mini_program/pages/me/index.js
@@ -6,6 +6,7 @@ const util = require('../../utils/util.js');
 const db = wx.cloud.database()
 const DB = wx.cloud.database().collection("travelnotes");
 const DB1 = wx.cloud.database().collection("stategies");
+const DB2 = wx.cloud.database().collection("system_strategy");
 Page({
 
   /**
@@ -29,12 +30,12 @@ Page({
     tabs: [
       {
         id: 0,
-        value: "景点",
+        value: "平台攻略",
         isActive: true
       },
       {
         id: 1,
-        value: "攻略",
+        value: "游客攻略",
         isActive: false
       },
       {
@@ -45,8 +46,10 @@ Page({
     ],
     list_travelnotes:[],
     list_strategy:[],
+    system_strategy:[],
     collect_travelnotes:0,
     collect_strategy:0,
+    collect_strategy_sys:0,
     user:[],
     
     dataList: [{
@@ -213,7 +216,6 @@ Page({
         console.error('[数据库] [查询记录] 失败：', err)
         },
         })
-
   },
 
   onReady(){
@@ -573,9 +575,9 @@ addlight:function(){
      })
   },
 
+  //收藏
   // 标题点击事件  从子组件传递过来  标签
   handleTabsItemChange(e) {
-    // console.log(e);
     // 1.获取被点击的标题
     const { index } = e.detail;
     // 2.修改原数组
@@ -591,6 +593,7 @@ addlight:function(){
   init() {
     var collect_travelnotes = 0;
     var collect_strategy = 0;
+    var collect_strategy_sys=0;
     //获取用户信息
     const that = this;
     wx.getUserInfo({
@@ -622,12 +625,12 @@ addlight:function(){
           tabs: [
             {
               id: 0,
-              value: "景点",
+              value: "平台攻略*"+this.data.collect_strategy_sys,
               isActive: true
             },
             {
               id: 1,
-              value: "攻略*"+this.data.collect_strategy,
+              value: "游客攻略*"+this.data.collect_strategy,
               isActive: false
             },
             {
@@ -644,7 +647,7 @@ addlight:function(){
       }
     })
 
-    //攻略
+    //游客攻略
     DB1.get({
       success: res => {
         res.data.forEach(v => {
@@ -664,12 +667,12 @@ addlight:function(){
           tabs: [
           {
             id: 0,
-            value: "景点",
+            value: "平台攻略*"+this.data.collect_strategy_sys,
             isActive: true
           },
           {
             id: 1,
-            value: "攻略*" + collect_strategy,
+            value: "游客攻略*" + collect_strategy,
             isActive: false
           },
           {
@@ -685,6 +688,48 @@ addlight:function(){
         console.log("查询失败", res);
       }
     })
+
+      //平台攻略
+      DB2.get({
+        success: res => {
+          res.data.forEach(v => {
+            console.log(v.good.length);
+            if (v.good.length != 0) {
+              v.good.forEach(u => {
+                if (u === this.data.user.nickName) {
+                  collect_strategy_sys = collect_strategy_sys + 1;
+                }
+              })
+            }
+          })
+          // console.log("查询成功", res.data);
+          this.setData({
+            system_strategy: res.data,
+            collect_strategy_sys: collect_strategy_sys,
+            tabs: [
+            {
+              id: 0,
+              value: "平台攻略*"+collect_strategy_sys,
+              isActive: true
+            },
+            {
+              id: 1,
+              value: "游客攻略*" + this.data.collect_strategy,
+              isActive: false
+            },
+            {
+              id: 2,
+              value: "游记*" + this.data.collect_travelnotes,
+              isActive: false
+            }
+          ],
+          })
+          console.log(this.data.collect_strategy);
+        },
+        file(res) {
+          console.log("查询失败", res);
+        }
+      })
   },
 
 
@@ -699,8 +744,6 @@ addlight:function(){
     this.init1();
     wx.stopPullDownRefresh();
   },
-
-
 
   //长按移除  游记
   longPress_good(e){
@@ -746,12 +789,12 @@ addlight:function(){
                      tabs: [
                       {
                         id: 0,
-                        value: "景点",
+                        value: "平台攻略*"+that.data.collect_strategy_sys,
                         isActive: false
                       },
                       {
                         id: 1,
-                        value: "攻略*" + that.data.collect_strategy,
+                        value: "游客攻略*" + that.data.collect_strategy,
                         isActive: false
                       },
                       {
@@ -791,7 +834,7 @@ addlight:function(){
   })
  },
 
-  //长按移除  攻略
+  //长按移除  游客攻略
   longPress_good_str(e) {
     console.log(e);
     const strategy_id = e.currentTarget.dataset.index
@@ -835,12 +878,12 @@ addlight:function(){
                     tabs: [
                       {
                         id: 0,
-                        value: "景点",
+                        value: "平台攻略*"+that.data.collect_strategy_sys,
                         isActive: false
                       },
                       {
                         id: 1,
-                        value: "攻略*" + collect_strategy,
+                        value: "游客攻略*" + collect_strategy,
                         isActive: true
                       },
                       {
@@ -854,6 +897,96 @@ addlight:function(){
                   console.log(that.data.list_travelnotes)
 
                   DB1.doc(strategy_id).update({
+                    data: {
+                      good: good,
+                      goodamount: goodamount,
+                    },
+                    success(res) {
+                      wx.showToast({
+                        title: '移除成功',
+                      })
+                    },
+                    fail: function (ms) {
+                      wx.showToast({
+                        title: '移除失败',
+                        icon: "none"
+                      })
+                    },
+                  })
+                } else if (t.cancel) {
+                  console.log('用户点击取消')
+                }
+              }
+            })
+          }
+        })
+      }
+    })
+  },
+
+  
+  //长按移除  游客攻略
+  longPress_good_sys(e) {
+    console.log(e);
+    const system_id = e.currentTarget.dataset.index
+    var system_strategy = this.data.system_strategy
+    var good;
+    var goodamount = e.currentTarget.dataset.goodamount
+    var that = this;
+    var collect_strategy_sys = this.data.collect_strategy_sys
+
+    wx.getUserInfo({
+      success: function (res) {
+        console.log(res);
+        system_strategy.forEach(v => {
+          // console.log(list_strategy)
+          if (system_id === v._id) {
+            wx.showModal({
+              title: '提示',
+              content: '是否确定移除收藏',
+              success(t) {
+                if (t.confirm) {
+                  console.log('用户点击确定', v.good.length);
+                  var i = 0;
+                  for (; i < v.good.length; i++) {
+                    // console.log("11111111111111")
+                    if (v.good[i] === res.userInfo.nickName) {
+                      console.log("取消收藏")
+                      v.good.splice(i, 1);
+                      console.log(v.good)
+                      v.goodamount = v.goodamount - 1
+                      good = v.good
+                      console.log(good)
+                      goodamount = v.goodamount
+                      collect_strategy_sys = collect_strategy_sys-1
+                      break;
+                    }
+                  }
+                  that.setData({
+                    system_strategy: system_strategy,
+                    collect_strategy_sys: collect_strategy_sys,
+                    tabs: [
+                      {
+                        id: 0,
+                        value: "平台攻略*"+collect_strategy_sys,
+                        isActive: false
+                      },
+                      {
+                        id: 1,
+                        value: "游客攻略*" +that.data.collect_strategy,
+                        isActive: true
+                      },
+                      {
+                        id: 2,
+                        value: "游记*" + that.data.collect_travelnotes,
+                        isActive: false
+                      }
+                    ],
+                  
+                  })
+                  console.log(that.data.list_travelnotes)
+
+                  DB2.doc(system_id).update({
                     data: {
                       good: good,
                       goodamount: goodamount,
@@ -928,19 +1061,14 @@ addlight:function(){
           tabs1: [
             {
               id: 0,
-              value: "景点",
+              value: "游客攻略*"+this.data.release_strategy,
               isActive: true
             },
             {
               id: 1,
-              value: "攻略*"+this.data.release_strategy,
-              isActive: false
-            },
-            {
-              id: 2,
               value: "游记*" + release_travelnotes,
               isActive: false
-            }
+            },
           ],
         })
         console.log("bbbbbbbbbbbbbbbbbb",this.data.release_travelnotes);
@@ -950,7 +1078,7 @@ addlight:function(){
       }
     })
 
-    //攻略
+    //游客攻略
     DB1.get({
       success: res => {
         // console.log("查询成功", res.data);
@@ -968,27 +1096,23 @@ addlight:function(){
           tabs1: [
             {
               id: 0,
-              value: "景点",
+              value: "游客攻略*" + release_strategy,
               isActive: true
             },
             {
               id: 1,
-              value: "攻略*" + release_strategy,
-              isActive: false
-            },
-            {
-              id: 2,
               value: "游记*" + this.data.release_travelnotes,
               isActive: false
-            }
+            },
           ],
         })
-       
       },
       file(res) {
         console.log("查询失败", res);
       }
     })
+
+   
   },
 
     //长按删除
@@ -1023,19 +1147,15 @@ addlight:function(){
                           tabs1: [
                             {
                               id: 0,
-                              value: "景点",
+                              value: "游客攻略*" + that.data.release_strategy,
                               isActive: false
                             },
                             {
                               id: 1,
-                              value: "攻略*" + that.data.release_strategy,
-                              isActive: false
-                            },
-                            {
-                              id: 2,
                               value: "游记*" +release_travelnotes,
                               isActive: true
-                            }
+                            },
+                           
                           ],
                         })
                         wx.showToast({
@@ -1059,7 +1179,7 @@ addlight:function(){
         }
       })
     },
-    //长按删除  攻略
+    //长按删除  游客攻略
     longPress_record_str(e) {
       console.log(e);
       const strategy_id = e.currentTarget.dataset.index
@@ -1091,16 +1211,11 @@ addlight:function(){
                           tabs1: [
                             {
                               id: 0,
-                              value: "景点",
-                              isActive: false
-                            },
-                            {
-                              id: 1,
-                              value: "攻略*" + release_strategy,
+                              value: "游客攻略*" + release_strategy,
                               isActive: true
                             },
                             {
-                              id: 2,
+                              id: 1,
                               value: "游记*" + that.data.release_travelnotes,
                               isActive: false
                             }
